21941a0887642161c4bbbd3ef489cf576560ac24
dessymol <dessymoldixon@gmail.com>
2026-03-07 23:08:16 +0530

    backend update

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,13 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-const BlogURL=process.env.blog_URL;
+const BlogURI=process.env.blog_URI;
 
 const connectDB=async()=>{
     try {
-        await mongoose.connect(BlogURL);
+        await mongoose.connect(BlogURI);
         console.log("Mongoose connected successfully");
     } catch (error) {
-        console.log("Mongoose connection faied",error);
+        console.log("Mongoose connection failed",error);
     }
 };
 module.exports=connectDB;
